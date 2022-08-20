c98c158a1a4a29b18ad337d85b52ba1ddc041064
mbolonzi-design <mbolonzi.design@gmail.com>
2022-08-20 23:23:54 +0300

    completed

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def badge_maker name 
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator names
+    badge_arry = names.map{ |name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms names
+    rooms = []
+    names.each_with_index do |name, index|
+        rooms << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    end
+    rooms
+end
+
+def printer names 
+    batch_badge_creator(names).each { |item| puts item}
+    assign_rooms(names).each { |item| puts item}
+end
